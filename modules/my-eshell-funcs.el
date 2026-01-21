38e1023e7990f9b42d4e1fff513b1f010d270fd8
Igor Rizhyi <igorrizhyi@gmail.com>
2026-01-21 12:57:39 +0200

    fix: better line-height

--- modules/my-eshell-funcs.el
+++ modules/my-eshell-funcs.el
@@ -170,7 +170,7 @@ If ENABLE-EAT is non-nil, enable eat-eshell-mode."
 
 ;;; Font size customization - smaller output, normal prompt
 (defface my-eshell-output-face
-  '((t (:height 0.85)))
+  '((t (:height 0.75)))
   "Face for eshell output (smaller than prompt).")
 
 (defvar-local my-eshell--in-command nil
@@ -218,13 +218,17 @@ If ENABLE-EAT is non-nil, enable eat-eshell-mode."
           (unless (string-match-p "[$#] $" text)
             ;; Create overlay with fixed boundaries (no extending)
             (let ((ov (make-overlay start end nil nil nil))
-                  (padding "  "))  ; 2 spaces left padding
-              (overlay-put ov 'face 'my-eshell-output-face)
+                  ;; Make padding have same small height
+                  (padding (propertize "  " 'face '(:height 0.85 :inherit nil))))
+              (overlay-put ov 'face '(:height 0.85 :inherit nil))
               (overlay-put ov 'line-prefix padding)
               (overlay-put ov 'wrap-prefix padding)
-              (overlay-put ov 'line-spacing 0)  ; no extra spacing between lines
               (overlay-put ov 'evaporate nil)
               (overlay-put ov 'my-eshell-output t)
+              ;; Apply line-height t to use actual text height, not inherited
+              (let ((inhibit-read-only t))
+                (with-silent-modifications
+                  (add-text-properties start end '(line-height t))))
               ;; Add newline before first output chunk
               (when my-eshell--first-output
                 (overlay-put ov 'before-string "\n")
