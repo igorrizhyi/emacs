30514664ae62c12747aaeb5ed70c3d7c3e201ee8
Igor Rizhyi <igorrizhyi@gmail.com>
2026-02-11 13:40:16 +0200

    fix: dired config update

--- modules/my-dired-extension.el
+++ modules/my-dired-extension.el
@@ -8,21 +8,25 @@
 (require 'dired)
 
 ;;;###autoload
-(defun my/dired-create-file (filename)
-  "Create a new file FILENAME in current dired directory."
+(defun my/dired-create (name)
+  "Create a new file or directory NAME in current dired directory.
+If NAME ends with /, create a directory. Otherwise create a file."
   (interactive
-   (list (read-file-name "Create file: " (dired-current-directory))))
-  (let ((file (expand-file-name filename)))
-    (if (file-exists-p file)
-        (user-error "File already exists: %s" file)
-      (write-region "" nil file)
-      (dired-add-file file)
+   (list (read-string "Create file/dir (end with / for directory): ")))
+  (let* ((path (expand-file-name name (dired-current-directory)))
+         (is-dir (string-suffix-p "/" name))
+         (target (if is-dir (directory-file-name path) path)))
+    (if (file-exists-p target)
+        (user-error "Already exists: %s" target)
+      (if is-dir
+          (make-directory target t)
+        (write-region "" nil target))
       (revert-buffer)
-      (dired-goto-file file))))
+      (dired-goto-file target))))
 
-;; Bind 'c' to create file in dired (evil normal state)
+;; Bind 'c' to create file/dir in dired (evil normal state)
 (after! evil-collection
-  (evil-define-key 'normal dired-mode-map "c" #'my/dired-create-file))
+  (evil-define-key 'normal dired-mode-map "c" #'my/dired-create))
 
 (provide 'my-dired-extension)
 ;;; my-dired-extension.el ends here
